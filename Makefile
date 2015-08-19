8bc6bb93c447af0ba66fc02989c4cd6273da4fff
Alistair <alistair@ghs.com>
2015-08-19 13:30:04 -0700

    Make test result depend on environemtn

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	$(CC) -o main.out main.c
+	$(CC) -o main.out main.c -DMAKE_PASS=$(MAKE_PASS)
 
 test:
 	./main.out
--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 
 int main() 
 {
+
     printf("hello world!\n");
+#if MAKE_PASS
+    printf("This should pass\n");
     return 0;
+#endif
 }
